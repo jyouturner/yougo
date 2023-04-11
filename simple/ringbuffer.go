1f9ccc06224f787c40d7b7f4c0cf9a994d39e239
Jerry You <jy2947@gmail.com>
2023-04-10 22:56:52 -0400

    add ring buffer

---
+++ simple/ringbuffer.go
@@ -0,0 +1,39 @@
+package yougo
+
+type RingBuffer struct {
+	data    []string
+	head    int
+	tail    int
+	size    int
+	maxSize int
+}
+
+func NewRingBuffer(maxSize int) *RingBuffer {
+	return &RingBuffer{
+		data:    make([]string, maxSize),
+		head:    0,
+		tail:    0,
+		size:    0,
+		maxSize: maxSize,
+	}
+}
+
+func (rb *RingBuffer) Put(item string) {
+	rb.data[rb.head] = item
+	rb.head = (rb.head + 1) % rb.maxSize
+	if rb.size < rb.maxSize {
+		rb.size++
+	} else {
+		rb.tail = (rb.tail + 1) % rb.maxSize
+	}
+}
+
+func (rb *RingBuffer) Contains(item string) bool {
+	for i := 0; i < rb.size; i++ {
+		index := (rb.tail + i) % rb.maxSize
+		if rb.data[index] == item {
+			return true
+		}
+	}
+	return false
+}
